370d7d7d111c9cc54d9b66e1e9f5078655908cbf
cjhowson <cjhowson@gmail.com>
2014-04-07 11:01:54 +0100

    TicketCollection unit tests

---
+++ src/KayakoRestApi.UnitTests/Tickets/TicketCollectionSerializationTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using KayakoRestApi.Core.Constants;
+using KayakoRestApi.Core.Tickets;
+using KayakoRestApi.UnitTests.Utilities;
+using NUnit.Framework;
+
+namespace KayakoRestApi.UnitTests.Tickets
+{
+	[TestFixture]
+	public class TicketCollectionSerializationTests
+	{
+		[Ignore]
+		public void TicketCollectionDeserialization()
+		{
+			var tickets =  new TicketCollection
+				{
+				};
+
+			var expectedTickets = XmlDataUtility.ReadFromFile<TicketCollection>("TestData/TicketCollection.xml");
+
+			AssertUtility.ObjectsEqual(expectedTickets, tickets);
+		}
+	}
+}
